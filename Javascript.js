a0ba13c6dbcbcb785f65559c52ad909825dcba11
aminautoparts <aminautoparts@yandex.ru>
2025-01-02 09:17:22 +0500

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,21 @@
+alert("Теперь Знаю");
+alert('Мир');
+var nam = 5;
+console.log("gh" + nam);
+(function() {
+    'use strict';
+  
+    // ...ваш код...
+  })()
+
+  let message = "Привет !!!";
+  alert(message);
+
+  let admin, name; // можно объявить две переменные через запятую
+
+name = "Джон";
+
+admin = name;
+
+alert( admin ); // "Джон"
+let ourPlanetName = "Земля"; // Лучше описать содержимое переменной подробнее
